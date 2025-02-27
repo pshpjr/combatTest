dbabf1a17b7b8e8edff4bda1720cc6a9ce7cf0cc
박성환 <pshpjr@hotmail.com>
2025-02-27 17:52:04 +0900

    fix: Fix image loading and drawing in main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
 
 	raylib::Window window(screenWidth, screenHeight, "raylib-cpp: display image");
 
-	raylib::Image image("resources/player.png"); // Load image data into CPU memory (RAM)
+	raylib::Image image("resources/player.png", 100, 50, 0); // Load image data into CPU memory (RAM)
 	raylib::Texture2D texture(image); // Create texture from image data
 
 	SetTargetFPS(60);
@@ -22,10 +22,6 @@ int main()
 	{
 		// Update
 		// TODO: Update your variables here
-		raylib::Image image2 = texture.GetTextureData();
-		image2.ColorTint(RED);
-		texture.Unload();
-		texture = raylib::Texture2D(image2);
 
 		// Draw
 		BeginDrawing();
@@ -33,11 +29,8 @@ int main()
 		ClearBackground(RAYWHITE);
 
 		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-		// texture.Draw(10, 10, WHITE); // Original draw call
-		Vector2 origin{ (float)texture.GetWidth() / 2, (float)texture.GetHeight() / 2 };
-		Rectangle sourceRec{ 0.0f, 0.0f, (float)texture.GetWidth(), (float)texture.GetHeight() };
-		Vector2 position{ 100, 100 };
-		DrawTexturePro(texture, sourceRec, Rectangle{ position.x, position.y, texture.GetWidth() * 2, texture.GetHeight() * 2 }, Vector2{ (float)texture.GetWidth(), (float)texture.GetHeight() }, 45.0f, WHITE);
+		texture.Draw(10, 10, WHITE);
+
 
 		EndDrawing();
 	}
