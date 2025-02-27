b9f5c541408be7da9c53584a92b68eeb0659f85f
박성환 <pshpjr@hotmail.com>
2025-02-27 17:42:41 +0900

    feat: Display image using raylib-cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,41 @@
+﻿#pragma comment(lib,"raylibdll.lib")
+#pragma comment(lib,"Winmm.lib")
+
 #include <iostream>
 #include <raylib-cpp.hpp>
 
-int main() {
-    // Initialization
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+int main()
+{
+	// Initialization
+	const int screenWidth = 800;
+	const int screenHeight = 450;
 
-    raylib::Window window(screenWidth, screenHeight, "raylib-cpp: display image");
+	raylib::Window window(screenWidth, screenHeight, "raylib-cpp: display image");
 
-    raylib::Image image("./resource/player.png");      // Load image data into CPU memory (RAM)
-    raylib::Texture2D texture(image);   // Create texture from image data
+	raylib::Image image("resources/player.png"); // Load image data into CPU memory (RAM)
+	raylib::Texture2D texture(image); // Create texture from image data
 
-    SetTargetFPS(60);
+	SetTargetFPS(60);
 
-    // Main game loop
-    while (!window.ShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        // TODO: Update your variables here
+	// Main game loop
+	while (!window.ShouldClose()) // Detect window close button or ESC key
+	{
+		// Update
+		// TODO: Update your variables here
 
-        // Draw
-        BeginDrawing();
+		// Draw
+		BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+		ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-            texture.Draw(10, 10, WHITE);
+		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+		texture.Draw(10, 10, WHITE);
 
-        EndDrawing();
-    }
+		EndDrawing();
+	}
 
-    // De-Initialization
-    texture.Unload();   // Texture unloading
+	// De-Initialization
+	texture.Unload(); // Texture unloading
 
-    return 0;
+	return 0;
 }
